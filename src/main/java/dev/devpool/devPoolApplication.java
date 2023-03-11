ae01e402ba2c2644ca826d848d4f9afbd2d3067d
codekim97 <kimtaeu4616@gmail.com>
2023-03-11 18:36:23 +0900

    first commit

---
+++ src/main/java/dev/devpool/devPoolApplication.java
@@ -0,0 +1,13 @@
+package dev.devpool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class devPoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(devPoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/devpool/DevpoolApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.devpool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevpoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

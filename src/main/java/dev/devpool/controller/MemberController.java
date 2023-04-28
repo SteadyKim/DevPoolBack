699e1174372421649794d95d923cd647a1cac87d
codekim97 <kimtaeu4616@gmail.com>
2023-04-28 10:04:40 +0900

    멤버 기타 수정

--- src/main/java/dev/devpool/controller/MemberController.java
+++ src/main/java/dev/devpool/controller/MemberController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
-@Tag(name="회원", description = "회원 관련 Controller")
 @RestController
 @RequestMapping("/api/member")
 @RequiredArgsConstructor

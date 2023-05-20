9a9e60a0eaf541293a47c0cf2e864c6c27f7d727
codekim97 <kimtaeu4616@gmail.com>
2023-05-20 15:11:28 +0900

    feat: MemberControllerTest 수정 테스트 작성
    fix: 리팩토링
    feat: 기능 개발
    debug: 오류 해결

--- src/test/java/dev/devpool/MemberControllerTest.java
+++ src/test/java/dev/devpool/MemberControllerTest.java
@@ -1,5 +1,10 @@
 package dev.devpool;
 
+import dev.devpool.domain.Member;
+import dev.devpool.dto.MemberDto;
+import dev.devpool.dto.common.CommonResponseDto;
+import dev.devpool.repository.MemberRepository;
+import dev.devpool.service.MemberService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -7,15 +12,36 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import javax.annotation.PostConstruct;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class MemberControllerTest {
 
+    @Autowired
+    private InitDb.InitService initService;
+
+        @PostConstruct
+        public void init() {
+            initService.ss();
+        }
         @Autowired
         private MockMvc mockMvc;
+
+        @Autowired
+        private MemberService memberService;
+
+        @Autowired
+        private MemberRepository memberRepository;
 
         @Test
         public void testGetMemberApi() throws Exception {
@@ -41,6 +67,36 @@ public class MemberControllerTest {
                 .andExpect(MockMvcResultMatchers.jsonPath("$.data.email").value("1234@naver.com"))
         ;
     }
+
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void testUpdateMemberWithAuthApi() throws Exception {
+
+        // API 호출 및 응답 확인
+        mockMvc.perform(MockMvcRequestBuilders
+                        .patch("/api/member/{memberId}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"name\":\"잉잉잉\",\"nickName\":\"귀욤둥이\",\"email\":\"1111@naver.com\",\"password\":\"1234\",\"imageUrl\":\"11211\"}")
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        mockMvc.perform(MockMvcRequestBuilders
+                        .get("/api/member/{memberId}", 1L)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.memberId").value(1L))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.email").value("1111@naver.com"))
+        ;
+
+    }
+
+
+
+
+
+
+
     }
 
 

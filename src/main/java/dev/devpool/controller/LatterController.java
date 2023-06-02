7f462ae6af91d4f595bc3a59657b49aec4ca2fba
codekim97 <kimtaeu4616@gmail.com>
2023-06-02 13:00:58 +0900

    feat: 쪽지 저장 api 구현
    fix: 리팩토링
    feat: 기능 개발
    debug: 오류 해결

--- src/main/java/dev/devpool/controller/LatterController.java
+++ src/main/java/dev/devpool/controller/LatterController.java
@@ -2,6 +2,8 @@ package dev.devpool.controller;
 
 import dev.devpool.dto.LatterDto;
 import dev.devpool.dto.common.CommonDataListResponseDto;
+import dev.devpool.dto.common.CommonDataResponseDto;
+import dev.devpool.dto.common.CommonResponseDto;
 import dev.devpool.service.LatterService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -10,10 +12,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -41,6 +40,21 @@ public class LatterController {
                 .message("멤버 쪽지 리스트 조회에 성공하였습니다.")
                 .dataList(latterDtoList)
                 .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseDto);
+    }
+
+    @Operation(summary = "회원의 쪽지 정보조회", description = "회원의 쪽지 정보를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "멤버 쪽지 조회 - 성공"),
+            @ApiResponse(responseCode = "404", description = "멤버 쪽지 조회 실패 - 멤버가 DB에 없습니다.")
+    })
+    @PostMapping("/latter")
+    public ResponseEntity<CommonResponseDto<Object>> saveLatter(@RequestBody LatterDto.Save latterDto) {
+
+        CommonResponseDto<Object> responseDto = latterService.join(latterDto);
+
+
 
         return ResponseEntity.status(HttpStatus.OK).body(responseDto);
     }

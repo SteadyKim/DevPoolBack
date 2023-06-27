d6f25a81a4d700c6477a6aa76874c40ea4018039
codekim97 <kimtaeu4616@gmail.com>
2023-06-27 13:47:03 +0900

    debug: ec2 timezone to asia/seoul
    fix: 리팩토링
    feat: 기능 개발
    debug: 오류 해결

--- src/main/java/dev/devpool/devPoolApplication.java
+++ src/main/java/dev/devpool/devPoolApplication.java
@@ -3,9 +3,16 @@ package dev.devpool;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class devPoolApplication {
 
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(devPoolApplication.class, args);
 	}
